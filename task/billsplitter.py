9e33ae80a04e31922b68143a01a2b980a8db3e63
ethan <ethanthomure@icloud.com>
2023-06-27 09:36:53 -0500

    stage 5/5

--- task/billsplitter.py
+++ task/billsplitter.py
@@ -17,35 +17,35 @@ class Party:
 
         self.set_party_count()
         print()
-        if self.party_count < 1:
+        if self.party_count < 2:
             print("No one is joining for the party")
         else:
             self.set_party_members()
             print()
             self.set_bill()
             print()
-            # self.disperse_bill()
             self.lucky()
+            self.disperse_bill()
 
     def set_party_count(self):
         self.party_count = int(input("Enter the number of friends joining (including you):\n"))
 
     def set_party_members(self):
-        if self.party_count < 1:
-            print("No one is joining for the party")
-        else:
-            print("Enter the name of every friend (including you), each on a new line:")
-            for i in range(self.party_count):
-                friend = Friend()
-                self.party_dict.update(friend.__dict__())
+        print("Enter the name of every friend (including you), each on a new line:")
+        for i in range(self.party_count):
+            friend = Friend()
+            self.party_dict.update(friend.__dict__())
 
     def set_bill(self):
         self.bill = int(input("Enter the total bill value:\n"))
 
     def disperse_bill(self):
-        dispersment = round(float(self.bill) / float(self.party_count), 2)
-        for key in self.party_dict.keys():
-            self.party_dict.update({key: dispersment})
+        if self.lucky_one == "":
+            disbursement = round(float(self.bill) / float(self.party_count), 2)
+            self.party_dict = {key: disbursement for key in self.party_dict.keys()}
+        else:
+            disbursement = round(float(self.bill) / float(self.party_count - 1), 2)
+            self.party_dict = {key: (disbursement if key != self.lucky_one else 0) for key in self.party_dict.keys()}
 
     def lucky(self):
         is_lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
@@ -62,3 +62,5 @@ class Party:
 
 if __name__ == "__main__":
     party = Party()
+    print()
+    party.print_party()
